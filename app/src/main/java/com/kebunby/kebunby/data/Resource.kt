59d847b371a5c51cf4f8fc9bb7a9fe5aabdd0db9
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-04-13 11:56:48 +0700

    Add unit test for LoginUserUseCase

--- app/src/main/java/com/kebunby/kebunby/data/Resource.kt
+++ app/src/main/java/com/kebunby/kebunby/data/Resource.kt
@@ -1,6 +1,6 @@
 package com.kebunby.kebunby.data
 
 sealed class Resource<T> {
-    data class Success<T>(val data: T?) : Resource<T>()
-    data class Error<T>(val message: String?) : Resource<T>()
+    data class Success<T>(val data: T? = null) : Resource<T>()
+    data class Error<T>(val message: String? = null) : Resource<T>()
 }
--- app/src/test/java/com/kebunby/kebunby/util/DummyDataGenerator.kt
+++ app/src/test/java/com/kebunby/kebunby/util/DummyDataGenerator.kt
@@ -1,9 +1,16 @@
 package com.kebunby.kebunby.util
 
 import com.kebunby.kebunby.data.model.UserCredential
+import com.kebunby.kebunby.data.model.request.LoginRequest
 
 fun generateUserCredential() =
     UserCredential(
         username = "george",
         accessToken = "abc"
+    )
+
+fun generateLoginRequest() =
+    LoginRequest(
+        username = "george",
+        password = "123"
     )
