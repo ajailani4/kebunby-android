eaf981979ed4ab11469a8d82e5cf89323b36a813
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-04-13 09:30:36 +0700

    Add unit test for LoginViewModel

---
+++ app/src/test/java/com/kebunby/kebunby/util/DummyDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.kebunby.kebunby.util
+
+import com.kebunby.kebunby.data.model.UserCredential
+
+fun generateUserCredential() =
+    UserCredential(
+        username = "george",
+        accessToken = "abc"
+    )
