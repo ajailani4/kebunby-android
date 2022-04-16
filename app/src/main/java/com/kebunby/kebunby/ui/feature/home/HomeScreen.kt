a38bf0651b8df089464a15446e5c49edd244d419
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-04-16 08:39:24 +0700

    Add HomeHeader

--- app/src/main/java/com/kebunby/kebunby/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/kebunby/kebunby/ui/feature/home/HomeScreen.kt
@@ -1,9 +1,83 @@
 package com.kebunby.kebunby.ui.feature.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.kebunby.kebunby.R
 
 @Composable
 fun HomeScreen() {
-    Text(text = "Home")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Column(modifier = Modifier.background(color = MaterialTheme.colors.primary)) {
+            HomeHeader()
+        }
+    }
+}
+
+@Composable
+fun HomeHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(
+                text = "${stringResource(id = R.string.hello)}, Anna",
+                color = MaterialTheme.colors.onPrimary,
+                fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = stringResource(id = R.string.have_a_nice_day),
+                color = MaterialTheme.colors.onPrimary,
+                style = MaterialTheme.typography.h3
+            )
+        }
+        Image(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape),
+            painter = /*rememberImagePainter()*/painterResource(id = R.drawable.img_default_ava),
+            contentDescription = "User ava"
+        )
+    }
+}
+
+@Composable
+fun HomeContent() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        shape = RoundedCornerShape(topEnd = 30.dp),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text(
+                text = stringResource(id = R.string.trending),
+                color = MaterialTheme.colors.primary,
+                fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.h3
+            )
+        }
+    }
 }
