d17a9759280bd75df045e925c22a894fb36e921a
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-04-18 09:28:09 +0700

    Change Text to ClickableText in TitleSection

--- app/src/main/java/com/kebunby/kebunby/ui/feature/home/component/TitleSection.kt
+++ app/src/main/java/com/kebunby/kebunby/ui/feature/home/component/TitleSection.kt
@@ -3,14 +3,20 @@ package com.kebunby.kebunby.ui.feature.home.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 import com.kebunby.kebunby.R
 import com.kebunby.kebunby.ui.theme.Grey
+import com.kebunby.kebunby.ui.theme.poppinsFamily
 
 @Composable
 fun TitleSection(
@@ -29,10 +35,19 @@ fun TitleSection(
         )
 
         if (isViewAllEnabled) {
-            Text(
-                text = stringResource(id = R.string.view_all),
-                color = Grey,
-                style = MaterialTheme.typography.body1
+            ClickableText(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Grey,
+                            fontFamily = poppinsFamily,
+                            fontSize = 14.sp
+                        )
+                    ) {
+                        append(stringResource(id = R.string.view_all))
+                    }
+                },
+                onClick = {}
             )
         }
     }
