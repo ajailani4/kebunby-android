cb9a50807d45b6bd9f1c493fa5759e8670017465
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-05-12 14:06:21 +0700

    Delete unused state classes

--- app/src/main/java/com/kebunby/kebunby/ui/feature/login/LoginState.kt
+++
@@ -1,9 +0,0 @@
-package com.kebunby.kebunby.ui.feature.login
-
-sealed class LoginState {
-    object Idle : LoginState()
-    object LoggingIn : LoginState()
-    object Success : LoginState()
-    data class Fail(val message: String?) : LoginState()
-    data class Error(val message: String?) : LoginState()
-}
