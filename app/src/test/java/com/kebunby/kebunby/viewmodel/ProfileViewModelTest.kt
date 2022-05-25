6f3fea411ef7b2bdd480fab9330216960e497c9e
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-05-25 22:26:29 +0700

    Update ProfileViewModelTest

--- app/src/test/java/com/kebunby/kebunby/viewmodel/ProfileViewModelTest.kt
+++ app/src/test/java/com/kebunby/kebunby/viewmodel/ProfileViewModelTest.kt
@@ -3,6 +3,7 @@ package com.kebunby.kebunby.viewmodel
 import com.kebunby.kebunby.data.Resource
 import com.kebunby.kebunby.data.model.User
 import com.kebunby.kebunby.domain.use_case.user.GetUserProfileUseCase
+import com.kebunby.kebunby.domain.use_case.user_credential.DeleteUserCredentialUseCase
 import com.kebunby.kebunby.domain.use_case.user_credential.GetUserCredentialUseCase
 import com.kebunby.kebunby.ui.common.UIState
 import com.kebunby.kebunby.ui.feature.profile.ProfileViewModel
@@ -11,9 +12,8 @@ import com.kebunby.kebunby.util.generateUser
 import com.kebunby.kebunby.util.generateUserCredential
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
-import org.junit.Assert.*
-
-import org.junit.Before
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -36,6 +36,9 @@ class ProfileViewModelTest {
     @Mock
     private lateinit var getUserProfileUseCase: GetUserProfileUseCase
 
+    @Mock
+    private lateinit var deleteUserCredentialUseCase: DeleteUserCredentialUseCase
+
     private lateinit var profileViewModel: ProfileViewModel
 
     @Test
@@ -54,7 +57,8 @@ class ProfileViewModelTest {
 
             profileViewModel = ProfileViewModel(
                 getUserCredentialUseCase,
-                getUserProfileUseCase
+                getUserProfileUseCase,
+                deleteUserCredentialUseCase
             )
 
             val userProfile = when (val userProfileState = profileViewModel.userProfileState) {
@@ -64,7 +68,11 @@ class ProfileViewModelTest {
 
             assertNotNull(userProfile)
             assertEquals("Username should be 'george'", "george", userProfile?.username)
-            assertEquals("Email should be 'george@email.com'", "george@email.com", userProfile?.email)
+            assertEquals(
+                "Email should be 'george@email.com'",
+                "george@email.com",
+                userProfile?.email
+            )
 
             verify(getUserCredentialUseCase).invoke()
             verify(getUserProfileUseCase).invoke(anyString())
@@ -87,7 +95,8 @@ class ProfileViewModelTest {
 
             profileViewModel = ProfileViewModel(
                 getUserCredentialUseCase,
-                getUserProfileUseCase
+                getUserProfileUseCase,
+                deleteUserCredentialUseCase
             )
 
             val isSuccess = when (val userProfileState = profileViewModel.userProfileState) {
