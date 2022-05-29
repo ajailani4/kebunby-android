e0fafb81916f36c60f4e73106710b10462bbf338
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-05-29 18:28:46 +0700

    Add CameraView

---
+++ app/src/main/java/com/kebunby/kebunby/ui/feature/camera/CameraScreen.kt
@@ -0,0 +1,8 @@
+package com.kebunby.kebunby.ui.feature.camera
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraScreen() {
+
+}
---
+++ app/src/main/java/com/kebunby/kebunby/ui/feature/camera/component/CameraView.kt
@@ -0,0 +1,32 @@
+package com.kebunby.kebunby.ui.feature.camera.component
+
+import android.content.Context
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.compose.runtime.*
+import java.io.File
+
+@Composable
+fun CameraView(
+    context: Context,
+    onImageCaptured: (File) -> Unit,
+    onError: (ImageCaptureException) -> Unit
+) {
+    var lensFacing by remember { mutableStateOf(CameraSelector.LENS_FACING_BACK) }
+    val imageCapture = remember { ImageCapture.Builder().build() }
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            val inputStream = context.contentResolver.openInputStream(uri)
+
+            if (inputStream != null) {
+
+            }
+        }
+    }
+}
